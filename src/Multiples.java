c4b1ace4d7d480cdc0ef1dac000bf7eb08234b4f
erinkim16 <125765924+erinkim16@users.noreply.github.com>
2023-09-14 19:34:41 -0400

    attempted some code to solve multiples method

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,6 +2,17 @@ import com.sun.jdi.IntegerValue;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println((999/3) + int(999/5) - int(999/15));
+        int multiples_so_far = 0;
+        int i = 0;
+        while (i < 1000) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                multiples_so_far += 1;
+                i++;
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.println(multiples_so_far);;
     }
 }
